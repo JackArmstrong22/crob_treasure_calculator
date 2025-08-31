3f4bc4cc72641ba87e9d754bdf52270b5758543c
JackArmstrong22 <armsjack@oregonstate.edu>
2025-08-31 15:39:10 -0700

    tested to see if playtime_calc correctly returns the point value of a lvl 1 treasure

--- tests/test_playtime_calc.py
+++ tests/test_playtime_calc.py
@@ -1,7 +1,13 @@
 import unittest
 from playtime_calc import playtime_calc
 
+LEVEL_ONE = 1
+
 
 class TestPlaytime(unittest.TestCase):
     def test1(self):
-        self.assertEqual(playtime_calc(0), "Invalid time!")
+        self.assertEqual(playtime_calc(LEVEL_ONE, 0), "Invalid time!")
+
+    def test2(self):
+        typewriter = 2026080
+        self.assertEqual(playtime_calc(LEVEL_ONE, 24), typewriter)
