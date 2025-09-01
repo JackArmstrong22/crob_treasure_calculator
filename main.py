86ead8e5e947add0313699ba79239a83ac87d284
JackArmstrong22 <armsjack@oregonstate.edu>
2025-08-31 19:29:05 -0700

    removed main from repo

--- main.py
+++
@@ -1,9 +0,0 @@
-from playtime_calc import playtime_calc
-
-if __name__ == "__main__":
-    result = playtime_calc(1, 24)
-    print(result)
-    for name, data in result:
-        treasure_obj = data["treasure"]
-        print(treasure_obj.name, treasure_obj.cooldown,
-              treasure_obj.point_per_jelly)
